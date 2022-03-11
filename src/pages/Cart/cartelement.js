20fce4130306458fddde933d9863f60bf05fc519
zaykha <zaykha@gmail.com>
2022-03-11 12:15:28 +0630

    json update

--- src/pages/Cart/cartelement.js
+++ src/pages/Cart/cartelement.js
@@ -2,13 +2,20 @@ import styled from "styled-components";
 
 export const CartMaincontainer = styled.div`
     width: 1200px;
+    border: 1px solid red;
+    margin: auto;
+
+    @media screen and (max-width:1200;){
+        width: 100%;
+    }
 `;
 export const Cartflexdiv = styled.div`
-    width: 1200px;
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
     margin: auto;
+    border: 1px solid green;
 `;
 export const CartSummarydiv = styled.div`
     width: 1200px;
@@ -23,4 +30,5 @@ export const CartTotaldiv = styled.div`
     align-items: center;
     justify-content: center;
     margin: auto;
+    border: 1px solid blue;
 `;
